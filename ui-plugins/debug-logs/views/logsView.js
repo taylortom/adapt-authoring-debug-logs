54a0bcd956ff003aaa9b01454a129f86a2121a86
Thomas Taylor <hello@tomtaylor.name>
2023-12-06 18:13:27 +0000

    Update log rendering

--- ui-plugins/debug-logs/views/logsView.js
+++ ui-plugins/debug-logs/views/logsView.js
@@ -1,18 +1,79 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require){
   var Backbone = require('backbone');
-  var ApiCollection = require('core/collections/apiCollection');
   var OriginView = require('core/views/originView');
 
   var LogsView = OriginView.extend({
     tagName: 'div',
     className: 'logs',
+    events: {
+      'change input': 'updateModel',
+      'keydown input': 'updateModel',
+      'change': 'fetchPage'
+    },
 
-    initialize: function(options) {
+    initialize: async function(options) {
       this.model = new Backbone.Model({
-        logs: new ApiCollection({ url: '/api/logs' })
+        levels: ['error', 'warn', 'success', 'info', 'debug'],
+        page: 1,
+        sort: { timestamp: -1 }
       });
+      this.listenTo(this.model, 'change', this.fetchPage)
+
       OriginView.prototype.initialize.apply(this, arguments);
+      
+      this.fetchPage();
+    },
+
+    waitForTimeout: async function() {
+      return new Promise(resolve => {
+        if(this.timeout) {
+          clearTimeout(this.timeout);
+        }
+        this.timeout = setTimeout(() => {
+          this.timeout = undefined;
+          resolve();
+        }, 500);
+      })
+    },
+
+    updateModel: async function(e) {
+      if(e.type === 'keydown') {
+        await this.waitForTimeout()
+      }
+      const levels = [];
+      const checkedInputs = this.$('.level input:checked');
+      checkedInputs.each(i => levels.push($(checkedInputs[i]).attr('id')))
+      
+      const attributes = {
+        levels,
+        sort: { timestamp: 1 }
+      };
+      const moduleName = this.$('#module').val();
+      if(moduleName) attributes.module = moduleName;
+      else this.model.unset('module');
+
+      this.model.set(attributes);
+    },
+
+    fetchPage: async function() {
+      try {
+        const query = [
+          `page=${this.model.get('page')}`,
+          `sort=${JSON.stringify(this.model.get('sort'))}`
+        ];
+        const data = {
+          level: { $in: this.model.get('levels') }
+        };
+        if(this.model.has('module')) {
+          data.module = this.model.get('module');
+        }
+        this.model.set('logs', await $.post(`/api/logs/query?${query.join('&')}`, data));
+        this.render();
+
+      } catch(e) {
+        console.log(e);
+      }
     }
   }, {
     template: 'logs'
