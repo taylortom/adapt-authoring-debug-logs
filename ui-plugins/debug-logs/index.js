b5cf3d8e07ace8782c8963a7ed4e0e2843aeea38
Tom Taylor <hello@tomtaylor.name>
2024-07-22 18:07:28 +0100

    Fix plugin init

--- ui-plugins/debug-logs/index.js
+++ ui-plugins/debug-logs/index.js
@@ -3,10 +3,12 @@ define(function(require) {
   const Origin = require('core/origin');
   const LogsView = require('./views/logsView');
 
-  Origin.trigger(`debug:addView`, { 
-    name: 'logs', 
-    icon: 'tasks', 
-    title: Origin.l10n.t('app.logs'), 
-    view: LogsView
+  Origin.on('debug:ready', () => {
+    Origin.trigger(`debug:addView`, { 
+      name: 'logs', 
+      icon: 'tasks', 
+      title: Origin.l10n.t('app.logs'), 
+      view: LogsView
+    })
   })
 });
