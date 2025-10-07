1b82a3252d9d74cf76d1cd5f8c305d6d8b0ed615
Tom Taylor <hello@tomtaylor.name>
2025-10-07 16:29:24 +0100

    Fix typo

--- ui-plugins/debug-logs/views/logsView.js
+++ ui-plugins/debug-logs/views/logsView.js
@@ -29,7 +29,7 @@ define(function(require){
     },
 
     renderLogs: function() {
-      $logs = $('.logs-container > .logs');
+      const $logs = $('.logs-container > .logs');
       $logs.empty();
       this.model.get('logs').forEach(l => $logs.append(Handlebars.partials.part_log(l)));
     },
